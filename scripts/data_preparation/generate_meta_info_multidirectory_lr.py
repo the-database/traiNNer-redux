0da3f19db8ef6ac504275afe298a012bed144c15
kodxana <romanrex19@int.pl>
2023-02-04 16:28:02 +0100

    Script to setup dataset with multiple dataroot_lq
    This will match HR frames with multiple LR folders

---
+++ scripts/data_preparation/generate_meta_info_multidirectory_lr.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+def get_lr_hr_pair(hr_folder, lr_folder):
+    hr_files = os.listdir(hr_folder)
+    lr_files = os.listdir(lr_folder)
+
+    hr_files.sort()
+    lr_files.sort()
+
+    lr_hr_pairs = []
+    for hr_file in hr_files:
+        base_name, _ = os.path.splitext(hr_file)
+        if base_name + ".png" in lr_files:
+            lr_hr_pairs.append((os.path.join(hr_folder, hr_file), os.path.join(lr_folder, base_name + ".png")))
+
+    return lr_hr_pairs
+
+def save_meta_info(meta_info_file, lr_hr_pairs):
+    with open(meta_info_file, "w") as f:
+        for lr, hr in lr_hr_pairs:
+            f.write("{}, {}\n".format(lr, hr))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hr_folder", type=str, default="HR")
+    parser.add_argument("--lr_folder", type=str, default="LR")
+    parser.add_argument("--meta_info", type=str, default="meta_info_DIV2K_sub_pair.txt")
+    args = parser.parse_args()
+
+    lr_hr_pairs = []
+    for lr_sub_folder in os.listdir(args.lr_folder):
+        lr_sub_folder_path = os.path.join(args.lr_folder, lr_sub_folder)
+        if os.path.isdir(lr_sub_folder_path):
+            lr_hr_pairs.extend(get_lr_hr_pair(args.hr_folder, lr_sub_folder_path))
+
+    save_meta_info(args.meta_info, lr_hr_pairs)
