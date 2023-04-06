85750c44a0db03b65543b756afa8ccc88af7e9f1
Joey Ballentine <joeyjballentine@gmail.com>
2023-04-06 01:14:18 -0400

    change root_dir

--- train.py
+++ train.py
@@ -215,5 +215,5 @@ def train_pipeline(root_path):
 
 
 if __name__ == '__main__':
-    root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
+    root_path = osp.abspath(osp.join(__file__, osp.pardir))
     train_pipeline(root_path)
