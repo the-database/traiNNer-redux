b5188b3ab2b50cf1f39ecee8e8a73b35aeb0e0e0
Joey Ballentine <joeyjballentine@gmail.com>
2023-04-06 19:33:21 -0400

    +

--- traiNNer/archs/2c2net_arch.py
+++ traiNNer/archs/2c2net_arch.py
@@ -149,12 +149,11 @@ class ResidualDenseBlock_5C(nn.Module):
 
     def forward(self, x):
         x1 = self.conv1(x)
-        x2 = self.conv2(torch.cat((x, x1), 1))+
+        x2 = self.conv2(torch.cat((x, x1), 1))
         x3 = self.conv3(torch.cat((x, x1, x2), 1))
         x4 = self.conv4(torch.cat((x, x1, x2, x3), 1))
         x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
         return x5 * 0.2 + x
-
 
 
 # 2x2x2 Conv Block
