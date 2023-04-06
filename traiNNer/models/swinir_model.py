25c81614908876dc75f088b69ec91706f3176d29
Joey Ballentine <joeyjballentine@gmail.com>
2023-04-06 00:49:23 -0400

    make swinir inherit srgan model

--- traiNNer/models/swinir_model.py
+++ traiNNer/models/swinir_model.py
@@ -2,11 +2,11 @@ import torch
 from torch.nn import functional as F
 
 from ..utils.registry import MODEL_REGISTRY
-from .sr_model import SRModel
+from .srgan_model import SRGANModel
 
 
 @MODEL_REGISTRY.register()
-class SwinIRModel(SRModel):
+class SwinIRModel(SRGANModel):
 
     def test(self):
         # pad to multiplication of window_size
