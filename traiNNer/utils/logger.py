d4ff38bdc211b6312448fb7901693d29fc2b6ff1
the-database <25811902+the-database@users.noreply.github.com>
2024-05-30 21:48:12 -0400

    add performance logging

--- traiNNer/utils/logger.py
+++ traiNNer/utils/logger.py
@@ -93,15 +93,13 @@ class MessageLogger():
 
         # time and estimated time
         if 'time' in log_vars.keys():
-            iter_time = log_vars.pop('time')
-            data_time = log_vars.pop('data_time')
+            iter_time = 1 / log_vars.pop('time')
 
             total_time = time.time() - self.start_time
             time_sec_avg = total_time / (current_iter - self.start_iter + 1)
             eta_sec = time_sec_avg * (self.max_iters - current_iter - 1)
             eta_str = str(datetime.timedelta(seconds=int(eta_sec)))
-            message += f'[eta: {eta_str}, '
-            message += f'time (data): {iter_time:.3f} ({data_time:.3f})] '
+            message += f'[performance: {iter_time:.3f}]] [eta: {eta_str}]'
 
         # other items, especially losses
         for k, v in log_vars.items():
