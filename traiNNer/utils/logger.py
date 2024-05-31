c9c1772b4311da0a327dc717a2934fc4fd61fa45
the-database <25811902+the-database@users.noreply.github.com>
2024-05-30 21:57:22 -0400

    logging fix

--- traiNNer/utils/logger.py
+++ traiNNer/utils/logger.py
@@ -94,12 +94,13 @@ class MessageLogger():
         # time and estimated time
         if 'time' in log_vars.keys():
             iter_time = 1 / log_vars.pop('time')
+            log_vars.pop('data_time')
 
             total_time = time.time() - self.start_time
             time_sec_avg = total_time / (current_iter - self.start_iter + 1)
             eta_sec = time_sec_avg * (self.max_iters - current_iter - 1)
             eta_str = str(datetime.timedelta(seconds=int(eta_sec)))
-            message += f'[performance: {iter_time:.3f}]] [eta: {eta_str}]'
+            message += f'[performance: {iter_time:.3f}] [eta: {eta_str}] '
 
         # other items, especially losses
         for k, v in log_vars.items():
