6bb72c157461a53bec5c650307b1fd32a6e1f7c6
the-database <25811902+the-database@users.noreply.github.com>
2024-06-01 02:10:50 -0400

    Create srvggmod_arch.py

---
+++ traiNNer/archs/srvggmod_arch.py
@@ -0,0 +1,77 @@
+from torch import nn as nn
+from torch.nn import functional as F
+
+from ..utils.registry import ARCH_REGISTRY
+
+
+@ARCH_REGISTRY.register(suffix='traiNNer')
+class SRVGGNetCompactMod(nn.Module):
+    """A compact VGG-style network structure for super-resolution.
+
+    It is a compact network structure, which performs upsampling in the last layer and no convolution is
+    conducted on the HR feature space.
+
+    Args:
+        num_in_ch (int): Channel number of inputs. Default: 3.
+        num_out_ch (int): Channel number of outputs. Default: 3.
+        num_feat (int): Channel number of intermediate features. Default: 64.
+        num_conv (int): Number of convolution layers in the body network. Default: 16.
+        upscale (int): Upsampling factor. Default: 4.
+        act_type (str): Activation type, options: 'relu', 'prelu', 'leakyrelu'. Default: prelu.
+    """
+
+    def __init__(self, num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu', conv2x2=False):
+        super(SRVGGNetCompactMod, self).__init__()
+        self.num_in_ch = num_in_ch
+        self.num_out_ch = num_out_ch
+        self.num_feat = num_feat
+        self.num_conv = num_conv
+        self.upscale = upscale
+        self.act_type = act_type
+        self.conv2x2 = conv2x2
+
+        def compact_conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+            if self.conv2x2:
+                return nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels, kernel_size=2, padding=1),
+                    nn.Conv2d(out_channels, out_channels, kernel_size=2, padding=0))
+            else:
+                return nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+
+        self.body = nn.ModuleList()
+        # the first conv
+        self.body.append(compact_conv_block(num_in_ch, num_feat, 3, 1, 1))
+        # the first activation
+        if act_type == 'relu':
+            activation = nn.ReLU(inplace=True)
+        elif act_type == 'prelu':
+            activation = nn.PReLU(num_parameters=num_feat)
+        elif act_type == 'leakyrelu':
+            activation = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        self.body.append(activation)
+
+        # the body structure
+        for _ in range(num_conv):
+            self.body.append(compact_conv_block(num_feat, num_feat, 3, 1, 1))
+            # activation
+            if act_type == 'relu':
+                activation = nn.ReLU(inplace=True)
+            elif act_type == 'prelu':
+                activation = nn.PReLU(num_parameters=num_feat)
+            elif act_type == 'leakyrelu':
+                activation = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+            self.body.append(activation)
+
+        # the last conv
+        self.body.append(compact_conv_block(num_feat, num_out_ch * upscale * upscale, 3, 1, 1))
+        # upsample
+        self.upsampler = nn.PixelShuffle(upscale)
+
+    def forward(self, x):
+        out = x
+        for i in range(0, len(self.body)):
+            out = self.body[i](out)
+
+        out = self.upsampler(out)
+
+        return out
