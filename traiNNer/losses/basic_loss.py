6d0511d27e336f65a85a94d0990099de523fe1d6
the-database <25811902+the-database@users.noreply.github.com>
2024-06-04 22:52:58 -0400

    Update basic_loss.py

--- traiNNer/losses/basic_loss.py
+++ traiNNer/losses/basic_loss.py
@@ -1060,17 +1060,11 @@ class MSSIMNeoLoss(nn.Module):
         return self.loss_weight * loss
 
     def msssim(self, x, y):
-        x = x.clamp(0, 1)
-        y = y.clamp(0, 1)
+        x = x.clamp(1e-12, 1)
+        y = y.clamp(1e-12, 1)
 
-        # cosine similarity
         if self.cosim:
-            similarity = nn.CosineSimilarity(dim=1, eps=1e-20)
-
-            x255 = torch.round(x * 255).clamp(1e-12, 255)
-            y255 = torch.round(y * 255).clamp(1e-12, 255)
-
-            cosine_term = (1 - similarity(x255, y255)).mean()
+            cosine_term = 1 - torch.round(self.similarity(x, y), decimals=20).mean()
 
         ms_components = []
         for i, w in enumerate((0.0448, 0.2856, 0.3001, 0.2363, 0.1333)):
