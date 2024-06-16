81cd2529d7ff1fec10fbe9d4ed1b85d2092261cb
the-database <25811902+the-database@users.noreply.github.com>
2024-06-16 14:09:52 -0400

    Update omnisr_arch.py

--- traiNNer/archs/omnisr_arch.py
+++ traiNNer/archs/omnisr_arch.py
@@ -4,5 +4,18 @@ from traiNNer.utils.registry import SPANDREL_REGISTRY
 
 
 @SPANDREL_REGISTRY.register()
-def omnisr(**kwargs):
-    return OmniSR(up_scale=Config.get_scale(), **kwargs)
+def omnisr(res_num=5,
+           block_num=1,
+           bias=True,
+           window_size=8,
+           pe=True,
+           ffn_bias=True,
+           **kwargs):
+    return OmniSR(up_scale=Config.get_scale(),
+                  res_num=res_num,
+                  block_num=block_num,
+                  bias=bias,
+                  window_size=window_size,
+                  pe=pe,
+                  ffn_bias=ffn_bias,
+                  **kwargs)
