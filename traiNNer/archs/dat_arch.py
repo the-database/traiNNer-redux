6c04a765d31e13cb63f716428756bc0ce060980d
the-database <25811902+the-database@users.noreply.github.com>
2024-06-16 13:37:28 -0400

    Update dat_arch.py

--- traiNNer/archs/dat_arch.py
+++ traiNNer/archs/dat_arch.py
@@ -4,8 +4,27 @@ from traiNNer.utils.registry import SPANDREL_REGISTRY
 
 
 @SPANDREL_REGISTRY.register()
-def dat(**kwargs):
-    return DAT(upscale=Config.get_scale(), **kwargs)
+def dat(in_chans=3,
+        img_size=64,
+        img_range=1.,
+        split_size=(8, 32),
+        depth=(6, 6, 6, 6, 6, 6),
+        embed_dim=180,
+        num_heads=(6, 6, 6, 6, 6, 6),
+        expansion_factor=4,
+        resi_connection='1conv',
+        **kwargs):
+    return DAT(upscale=Config.get_scale(),
+               in_chans=in_chans,
+               img_size=img_size,
+               img_range=img_range,
+               split_size=split_size,
+               depth=depth,
+               embed_dim=embed_dim,
+               num_heads=num_heads,
+               expansion_factor=expansion_factor,
+               resi_connection=resi_connection,
+               **kwargs)
 
 
 @SPANDREL_REGISTRY.register()
