edade802cb10b40d2ea155f591c06867ff782888
the-database <25811902+the-database@users.noreply.github.com>
2024-06-16 13:32:13 -0400

    Update srformer_arch.py

--- traiNNer/archs/srformer_arch.py
+++ traiNNer/archs/srformer_arch.py
@@ -4,23 +4,28 @@ from traiNNer.utils.registry import SPANDREL_REGISTRY
 
 
 @SPANDREL_REGISTRY.register()
-def srformer(**kwargs):
-    return SRFormer(upscale=Config.get_scale(), **kwargs)
-
-
-@SPANDREL_REGISTRY.register()
-def srformer_medium(**kwargs):
+def srformer(in_chans=3,
+             img_size=48,
+             window_size=24,
+             img_range=1.,
+             depths=(6, 6, 6, 6, 6, 6),
+             embed_dim=180,
+             num_heads=(6, 6, 6, 6, 6, 6),
+             mlp_ratio=2,
+             upsampler='pixelshuffle',
+             resi_connection='1conv',
+             **kwargs):
     return SRFormer(upscale=Config.get_scale(),
-                    in_chans=3,
-                    img_size=48,
-                    window_size=24,
-                    img_range=1.,
-                    depths=[6, 6, 6, 6, 6, 6],
-                    embed_dim=180,
-                    num_heads=[6, 6, 6, 6, 6, 6],
-                    mlp_ratio=2,
-                    upsampler='pixelshuffle',
-                    resi_connection='1conv',
+                    in_chans=in_chans,
+                    img_size=img_size,
+                    window_size=window_size,
+                    img_range=img_range,
+                    depths=depths,
+                    embed_dim=embed_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    upsampler=upsampler,
+                    resi_connection=resi_connection,
                     **kwargs)
 
 
