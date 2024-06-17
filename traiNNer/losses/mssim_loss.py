2a902279989a6cb160f74bbfab29b4d304288ab6
the-database <25811902+the-database@users.noreply.github.com>
2024-06-17 14:20:58 -0400

    Update mssim_loss.py

--- traiNNer/losses/mssim_loss.py
+++ traiNNer/losses/mssim_loss.py
@@ -1,8 +1,9 @@
 import math
+from collections.abc import Sequence
 
 import torch
-from torch import nn
-from torch.nn import functional as F
+from torch import SymInt, nn
+from torch.nn import functional as F  # noqa: N812
 from traiNNer.utils.registry import LOSS_REGISTRY
 
 ####################################
@@ -12,7 +13,13 @@ from traiNNer.utils.registry import LOSS_REGISTRY
 
 
 class GaussianFilter2D(nn.Module):
-    def __init__(self, window_size=11, in_channels=3, sigma=1.5, padding=None) -> None:
+    def __init__(
+        self,
+        window_size: int = 11,
+        in_channels: int = 3,
+        sigma: float = 1.5,
+        padding: int | SymInt | Sequence[int | SymInt] = None,
+    ) -> None:
         """2D Gaussian Filer
 
         Args:
@@ -35,20 +42,20 @@ class GaussianFilter2D(nn.Module):
             name="gaussian_window", tensor=kernel.repeat(in_channels, 1, 1, 1)
         )
 
-    def _get_gaussian_window1d(self):
+    def _get_gaussian_window1d(self) -> torch.Tensor:
         sigma2 = self.sigma * self.sigma
         x = torch.arange(-(self.window_size // 2), self.window_size // 2 + 1)
         w = torch.exp(-0.5 * x**2 / sigma2)
         w = w / w.sum()
         return w.reshape(1, 1, 1, self.window_size)
 
-    def _get_gaussian_window2d(self, gaussian_window_1d):
+    def _get_gaussian_window2d(self, gaussian_window_1d: torch.Tensor) -> torch.Tensor:
         w = torch.matmul(
             gaussian_window_1d.transpose(dim0=-1, dim1=-2), gaussian_window_1d
         )
         return w
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = F.conv2d(
             input=x,
             weight=self.gaussian_window,
@@ -63,16 +70,16 @@ class GaussianFilter2D(nn.Module):
 class MSSIMLoss(nn.Module):
     def __init__(
         self,
-        window_size=11,
-        in_channels=3,
-        sigma=1.5,
-        K1=0.01,
-        K2=0.03,
-        L=1,
-        padding=None,
-        cosim=True,
-        cosim_lambda=5,
-        loss_weight=1.0,
+        window_size: int = 11,
+        in_channels: int = 3,
+        sigma: float = 1.5,
+        k1: float = 0.01,
+        k2: float = 0.03,
+        l: int = 1,
+        padding: int | SymInt | Sequence[int | SymInt] = None,
+        cosim: bool = True,
+        cosim_lambda: int = 5,
+        loss_weight: float = 1.0,
     ) -> None:
         """Adapted from 'A better pytorch-based implementation for the mean structural
             similarity. Differentiable simpler SSIM and MS-SSIM.':
@@ -84,8 +91,8 @@ class MSSIMLoss(nn.Module):
             window_size (int): The window size of the gaussian filter. Defaults to 11.
             in_channels (int, optional): The number of channels of the 4d tensor. Defaults to False.
             sigma (float): The sigma of the gaussian filter. Defaults to 1.5.
-            K1 (float): K1 of MSSIM. Defaults to 0.01.
-            K2 (float): K2 of MSSIM. Defaults to 0.03.
+            k1 (float): k1 of MSSIM. Defaults to 0.01.
+            k2 (float): k2 of MSSIM. Defaults to 0.03.
             L (int): The dynamic range of the pixel values (255 for 8-bit grayscale images). Defaults to 1.
             padding (int, optional): The padding of the gaussian filter. Defaults to None. If it is set to None,
                 the filter will use window_size//2 as the padding. Another common setting is 0.
@@ -96,8 +103,8 @@ class MSSIMLoss(nn.Module):
         super().__init__()
 
         self.window_size = window_size
-        self.C1 = (K1 * L) ** 2  # equ 7 in ref1
-        self.C2 = (K2 * L) ** 2  # equ 7 in ref1
+        self.C1 = (k1 * l) ** 2  # equ 7 in ref1
+        self.C2 = (k2 * l) ** 2  # equ 7 in ref1
         self.cosim = cosim
         self.cosim_lambda = cosim_lambda
         self.loss_weight = loss_weight
@@ -111,7 +118,7 @@ class MSSIMLoss(nn.Module):
         )
 
     @torch.cuda.amp.custom_fwd(cast_inputs=torch.float32)
-    def forward(self, x, y):
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         """x, y (Tensor): tensors of shape (N,C,H,W)
         Returns: Tensor
         """
@@ -127,7 +134,7 @@ class MSSIMLoss(nn.Module):
 
         return self.loss_weight * loss
 
-    def msssim(self, x, y):
+    def msssim(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         x = x.clamp(1e-12, 1)
         y = y.clamp(1e-12, 1)
 
@@ -157,21 +164,23 @@ class MSSIMLoss(nn.Module):
 
         return msssim
 
-    def _ssim(self, x, y):
+    def _ssim(
+        self, x: torch.Tensor, y: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         mu_x = self.gaussian_filter(x)  # equ 14
         mu_y = self.gaussian_filter(y)  # equ 14
         sigma2_x = self.gaussian_filter(x * x) - mu_x * mu_x  # equ 15
         sigma2_y = self.gaussian_filter(y * y) - mu_y * mu_y  # equ 15
         sigma_xy = self.gaussian_filter(x * y) - mu_x * mu_y  # equ 16
 
-        A1 = 2 * mu_x * mu_y + self.C1
-        A2 = 2 * sigma_xy + self.C2
-        B1 = mu_x.pow(2) + mu_y.pow(2) + self.C1
-        B2 = sigma2_x + sigma2_y + self.C2
+        a1 = 2 * mu_x * mu_y + self.C1
+        a2 = 2 * sigma_xy + self.C2
+        b1 = mu_x.pow(2) + mu_y.pow(2) + self.C1
+        b2 = sigma2_x + sigma2_y + self.C2
 
         # equ 12, 13 in ref1
-        l = A1 / B1
-        cs = A2 / B2
+        l = a1 / b1
+        cs = a2 / b2
         ssim = l * cs
 
         return ssim, cs
