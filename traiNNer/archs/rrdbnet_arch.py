fc8e4b721c951a23509f77140f99255109ce5447
the-database <25811902+the-database@users.noreply.github.com>
2024-06-17 12:31:52 -0400

    Update rrdbnet_arch.py

--- traiNNer/archs/rrdbnet_arch.py
+++ traiNNer/archs/rrdbnet_arch.py
@@ -1,13 +1,13 @@
 from spandrel.architectures.ESRGAN import RRDBNet
 from traiNNer.utils.config import Config
-from traiNNer.utils.registry import SPANDREL_REGISTRY
 from traiNNer.utils.logger import get_root_logger
+from traiNNer.utils.registry import SPANDREL_REGISTRY
 
 pixel_unshuffle_scales = (1, 2)
 
 
 @SPANDREL_REGISTRY.register()
-def esrgan(use_pixel_unshuffle=False, in_nc=3, **kwargs) -> RRDBNet:
+def esrgan(use_pixel_unshuffle: bool = False, in_nc: int = 3, **kwargs) -> RRDBNet:
     scale = Config.get_scale()
 
     if use_pixel_unshuffle:
@@ -16,11 +16,16 @@ def esrgan(use_pixel_unshuffle=False, in_nc=3, **kwargs) -> RRDBNet:
             return RRDBNet(scale=4, in_nc=in_nc, shuffle_factor=scale, **kwargs)
         else:
             logger = get_root_logger()
-            logger.warning(f"Pixel unshuffle option is ignored since scale is not in {pixel_unshuffle_scales}")
+            logger.warning(
+                "Pixel unshuffle option is ignored since scale is not in %s",
+                str(pixel_unshuffle_scales),
+            )
 
     return RRDBNet(scale=scale, **kwargs)
 
 
 @SPANDREL_REGISTRY.register()
-def esrgan_lite(use_pixel_unshuffle=False, **kwargs) -> RRDBNet:
-    return esrgan(use_pixel_unshuffle=use_pixel_unshuffle, num_filters=32, num_blocks=12, **kwargs)
+def esrgan_lite(use_pixel_unshuffle: bool = False, **kwargs) -> RRDBNet:
+    return esrgan(
+        use_pixel_unshuffle=use_pixel_unshuffle, num_filters=32, num_blocks=12, **kwargs
+    )
