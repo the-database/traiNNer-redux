5727a7acd88951a4d84568b347752e2ef39b6331
the-database <25811902+the-database@users.noreply.github.com>
2024-06-23 20:00:16 -0400

    regenerate configs

--- scripts/options/generate_default_options.py
+++ scripts/options/generate_default_options.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from os import path as osp
 from typing import TypedDict
 
@@ -40,14 +39,34 @@ for arch in archs:
     # print(folder_name, folder_path)
     os.makedirs(folder_path, exist_ok=True)
 
-    for name in arch["names"]:
-        for scale in arch["scales"]:
-            shutil.copy(
-                osp.normpath(osp.join(__file__, osp.pardir, "./default_options_paired.yml")),
-                osp.join(folder_path, f"{scale}x_{name}.yml"),
-            )
+    template_path = osp.normpath(
+        osp.join(__file__, osp.pardir, "./default_options_paired.yml")
+    )
 
-            shutil.copy(
-                osp.normpath(osp.join(__file__, osp.pardir, "./default_options_paired.yml")),
-                osp.join(folder_path, f"{scale}x_{name}_OTF.yml"),
-            )
+    with open(template_path) as f:
+        template = f.read()
+
+        default_scale = 4
+
+        template = template.replace(
+            "scale: %scale%",
+            f"scale: {default_scale}  # {', '.join([str(x) for x in arch['scales']])}",
+        )
+
+        arch_type_str = f"type: {arch['names'][0]}"
+        if len(arch["names"]) > 1:
+            arch_type_str += f"  # {', '.join([str(x) for x in arch['names']])}"
+
+        template = template.replace(
+            "type: %archname%",
+            arch_type_str,
+        )
+
+        template = template.replace("%archname%", f"{arch['names'][0]}")
+        template = template.replace("%scale%", f"{default_scale}x")
+
+        with open(osp.join(folder_path, f"{folder_name}.yml"), mode="w") as fw:
+            fw.write(template)
+
+        with open(osp.join(folder_path, f"{folder_name}_OTF.yml"), mode="w") as fw:
+            fw.write(template)
