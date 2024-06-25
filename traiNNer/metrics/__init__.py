51b0564dc4e9d5fb025bcd30be35fa767d3a2c75
the-database <25811902+the-database@users.noreply.github.com>
2024-06-24 23:04:16 -0400

    dists metric fix

--- traiNNer/metrics/__init__.py
+++ traiNNer/metrics/__init__.py
@@ -1,10 +1,11 @@
 from copy import deepcopy
 from typing import Any
 
-from ..utils.registry import METRIC_REGISTRY
-from .psnr_ssim import calculate_psnr, calculate_ssim
+from traiNNer.metrics.dists import calculate_dists
+from traiNNer.metrics.psnr_ssim import calculate_psnr, calculate_ssim
+from traiNNer.utils.registry import METRIC_REGISTRY
 
-__all__ = ["calculate_psnr", "calculate_ssim"]
+__all__ = ["calculate_psnr", "calculate_ssim", "calculate_dists"]
 
 
 def calculate_metric(data: dict[str, Any], opt: dict[str, Any]) -> float:
