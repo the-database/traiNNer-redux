2ab74dbd9e267ac8e3934b880e91b6601db8a52b
the-database <25811902+the-database@users.noreply.github.com>
2024-07-03 00:14:59 -0400

    Update test_losses.py

--- tests/test_losses/test_losses.py
+++ tests/test_losses/test_losses.py
@@ -1,7 +1,10 @@
 from collections.abc import Callable
+from typing import TypedDict
 
 import pytest
 import torch
+from torch import Tensor, nn
+from traiNNer.losses.adists_loss import ADISTSLoss
 from traiNNer.losses.basic_loss import (
     CharbonnierLoss,
     ColorLoss,
@@ -13,52 +16,50 @@ from traiNNer.losses.dists_loss import DISTSLoss
 from traiNNer.losses.mssim_loss import MSSIMLoss
 from traiNNer.losses.perceptual_loss import PerceptualLoss
 
-
-class TestLosses:
-    N = 1
-    H = 256
-    W = 256
-
-    black_image = (
-        torch.tensor([[[0, 0, 0]]], dtype=torch.float32)
-        .repeat(N, H, W, 1)
-        .permute(0, 3, 1, 2)
-    )
-    white_image = (
-        torch.tensor([[[1, 1, 1]]], dtype=torch.float32)
-        .repeat(N, H, W, 1)
-        .permute(0, 3, 1, 2)
-    )
-    red_image = (
-        torch.tensor([[[1, 0, 0]]], dtype=torch.float32)
-        .repeat(N, H, W, 1)
-        .permute(0, 3, 1, 2)
-    )
-    green_image = (
-        torch.tensor([[[0, 1, 0]]], dtype=torch.float32)
-        .repeat(N, H, W, 1)
-        .permute(0, 3, 1, 2)
-    )
-
-    mssim_loss = MSSIMLoss()
-    l1_loss = L1Loss()
-    luma_loss = LumaLoss(criterion="charbonnier")
-    charbonnier_loss = CharbonnierLoss()
-    perceptual_loss = PerceptualLoss(
+LOSS_FUNCTIONS = [
+    MSSIMLoss(),
+    L1Loss(),
+    ADISTSLoss(),
+    DISTSLoss(),
+    PerceptualLoss(
         layer_weights={
             "conv1_2": 0.1,
             "conv2_2": 0.1,
             "conv3_4": 1,
             "conv4_4": 1,
             "conv5_4": 1,
-        }
-    )
-    color_loss = ColorLoss(criterion="charbonnier")
-    dists_loss = DISTSLoss()
-    mse_loss = MSELoss()
+        },
+        criterion="charbonnier",
+    ),
+    ColorLoss(criterion="charbonnier"),
+    LumaLoss(criterion="charbonnier"),
+    CharbonnierLoss(),
+    MSELoss(),
+]
 
-    eps = 1e-5  # torch.finfo(torch.float32).eps
+EPSILON = 1e-5  # torch.finfo(torch.float32).eps
 
+
+class TestLossData(TypedDict):
+    device: str
+    black_image: Tensor
+    dtype: torch.dtype
+
+
+@pytest.fixture
+def data() -> TestLossData:
+    device = "cpu"
+    input_shape = (1, 3, 16, 16)
+    use_fp16 = False
+    dtype = torch.float16 if use_fp16 else torch.float32
+    return {
+        "device": device,
+        "dtype": dtype,
+        "black_image": torch.zeros(input_shape, device=device, dtype=dtype),
+    }
+
+
+class TestLosses:
     @pytest.mark.parametrize("loss_class", [L1Loss, MSELoss, CharbonnierLoss])
     def test_pixellosses(self, loss_class: Callable) -> None:
         """Test loss: pixel losses"""
@@ -95,46 +96,16 @@ class TestLosses:
     @pytest.mark.parametrize(
         "loss_fn",
         [
-            mssim_loss,
-            l1_loss,
-            luma_loss,
-            mse_loss,
-            charbonnier_loss,
-            perceptual_loss,
-            color_loss,
-            dists_loss,
+            pytest.param(loss_fn, id=f"{loss_fn}")
+            for loss_fn in LOSS_FUNCTIONS
         ],
     )
-    def test_black_vs_black(self, loss_fn: Callable) -> None:
-        loss_value = loss_fn(self.black_image, self.black_image)
+    def test_black_vs_black(self, data: TestLossData, loss_fn: nn.Module) -> None:
+        print(loss_fn)
+        loss_value = loss_fn(data["black_image"], data["black_image"])
         print(loss_value)
 
         if type(loss_value) is tuple:
-            assert loss_value[0] <= self.eps
+            assert loss_value[0] <= EPSILON
         else:
-            assert loss_value <= self.eps
-
-    @pytest.mark.parametrize(
-        "loss_fn",
-        [
-            mssim_loss,
-            l1_loss,
-            luma_loss,
-            mse_loss,
-            charbonnier_loss,
-            perceptual_loss,
-            color_loss,
-            dists_loss,
-        ],
-    )
-    def test_black_vs_black_float64(self, loss_fn: Callable) -> None:
-        loss_value = loss_fn(
-            self.black_image.to(dtype=torch.float64),
-            self.black_image.to(dtype=torch.float64),
-        )
-        print(loss_value)
-
-        if type(loss_value) is tuple:
-            assert loss_value[0] <= self.eps
-        else:
-            assert loss_value <= self.eps
+            assert loss_value <= EPSILON
