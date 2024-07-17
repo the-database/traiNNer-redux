9cd1b2894ae8a24aa68cd0f48a882f223eac2f40
the-database <25811902+the-database@users.noreply.github.com>
2024-07-17 18:21:53 -0400

    Update convert_to_onnx.py

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,17 +1,13 @@
-import time
+# TODO
+# if __name__ == "__main__":
+#     torch_model = dat_2(scale=4).cuda()
+#     torch_input = torch.randn(1, 1, 32, 32, device="cuda")
+#     start_time = time.time()
+#     # onnx_program = torch.onnx.dynamo_export(torch_model, torch_input)
+#     torch.onnx.export(
+#         torch_model, torch_input, "dat_2.onnx", verbose=False, opset_version=17
+#     )
 
-import torch
-from traiNNer.archs.dat_arch import dat_2
-
-if __name__ == "__main__":
-    torch_model = dat_2(scale=4).cuda()
-    torch_input = torch.randn(1, 1, 32, 32, device="cuda")
-    start_time = time.time()
-    # onnx_program = torch.onnx.dynamo_export(torch_model, torch_input)
-    torch.onnx.export(
-        torch_model, torch_input, "dat_2.onnx", verbose=False, opset_version=17
-    )
-
-    end_time = time.time()
-    print(f"{end_time - start_time} seconds")
-    # onnx_program.save("dat_2.onnx")
+#     end_time = time.time()
+#     print(f"{end_time - start_time} seconds")
+#     # onnx_program.save("dat_2.onnx")
