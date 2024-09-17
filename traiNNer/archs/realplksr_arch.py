fb22bc620ad1c35fd64ea294b199382c33eb1b30
the-database <25811902+the-database@users.noreply.github.com>
2024-09-16 21:08:41 -0400

    Update realplksr_arch.py

--- traiNNer/archs/realplksr_arch.py
+++ traiNNer/archs/realplksr_arch.py
@@ -1,8 +1,14 @@
+from typing import Literal
+
 from spandrel.architectures.PLKSR import RealPLKSR
 
 from traiNNer.utils.registry import ARCH_REGISTRY
 
 
 @ARCH_REGISTRY.register()
-def realplksr(scale: int = 4, **kwargs) -> RealPLKSR:
-    return RealPLKSR(upscaling_factor=scale, **kwargs)
+def realplksr(
+    scale: int = 4,
+    upsampler: Literal["dysample", "pixelshuffle"] = "pixelshuffle",
+    **kwargs,
+) -> RealPLKSR:
+    return RealPLKSR(upscaling_factor=scale, dysample=upsampler == "dysample", **kwargs)
