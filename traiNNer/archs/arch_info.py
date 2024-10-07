61df408e95a5b202e647a3f6bc030a0ae692ba32
the-database <25811902+the-database@users.noreply.github.com>
2024-10-07 12:40:44 -0400

    Update arch_info.py

--- traiNNer/archs/arch_info.py
+++ traiNNer/archs/arch_info.py
@@ -1,4 +1,6 @@
 # TODO refactor
+from typing import Literal
+
 ARCHS_WITHOUT_FP16 = {
     "atd",
     "atd_light",
@@ -9,7 +11,6 @@ ARCHS_WITHOUT_FP16 = {
     "hat_l",
     "hat_m",
     "hat_s",
-    "realplksr",
     "rgt",
     "rgt_s",
     "srformer",
@@ -20,4 +21,42 @@ ARCHS_WITHOUT_FP16 = {
     "hit_srf",
     "hit_sng",
     "hit_sir",
+}
+
+# Urban100
+OFFICIAL_METRICS: dict[
+    str, dict[int, dict[Literal["psnr", "ssim", "dists"], float]]
+] = {
+    "atd": {
+        2: {"psnr": 34.73, "ssim": 0.9476, "dists": 0},
+        3: {"psnr": 30.52, "ssim": 0.8924, "dists": 0},
+        4: {"psnr": 28.22, "ssim": 0.8414, "dists": 0},
+    },
+    "dat_2": {
+        2: {"psnr": 34.31, "ssim": 0.9457, "dists": 0},
+        3: {"psnr": 30.13, "ssim": 0.8878, "dists": 0},
+        4: {"psnr": 27.86, "ssim": 0.8341, "dists": 0},
+    },
+    "drct_l": {
+        2: {"psnr": 35.17, "ssim": 0.9516, "dists": 0},
+        3: {"psnr": 31.14, "ssim": 0.9004, "dists": 0},
+        4: {"psnr": 28.70, "ssim": 0.8508, "dists": 0},
+    },
+    "hat_l": {
+        2: {"psnr": 35.09, "ssim": 0.9513, "dists": 0},
+        3: {"psnr": 30.92, "ssim": 0.8981, "dists": 0},
+        4: {"psnr": 28.60, "ssim": 0.8498, "dists": 0},
+    },
+    "plksr": {
+        2: {"psnr": 33.36, "ssim": 0.9395, "dists": 0},
+        3: {"psnr": 29.10, "ssim": 0.8713, "dists": 0},
+        4: {"psnr": 26.85, "ssim": 0.8097, "dists": 0},
+    },
+    "span": {
+        2: {"psnr": 32.24, "ssim": 0.9294, "dists": 0},
+        4: {"psnr": 26.18, "ssim": 0.7879, "dists": 0},
+    },
+    "esrgan": {
+        4: {"psnr": 27.03, "ssim": 0.8153, "dists": 0},
+    },
 }
