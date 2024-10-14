092f8d8b34ada28ae60815351486c1a6ab0888dc
the-database <25811902+the-database@users.noreply.github.com>
2024-10-13 20:32:44 -0400

    log peak vram

--- traiNNer/utils/logger.py
+++ traiNNer/utils/logger.py
@@ -59,11 +59,11 @@ class MessageLogger:
 
     Args:
         opt (dict): Config. It contains the following keys:
-            name (str): Exp name.
+            name (str): Experiment name.
             logger (dict): Contains 'print_freq' (str) for logger interval.
-            train (dict): Contains 'total_iter' (int) for total iters.
+            train (dict): Contains 'total_iter' (int) for total iterations.
             use_tb_logger (bool): Use tensorboard logger.
-        start_iter (int): Start iter. Default: 1.
+        start_iter (int): Start iteration. Default: 1.
         tb_logger (obj:`tb_logger`): Tensorboard logger. Default: None.
     """
 
@@ -75,6 +75,7 @@ class MessageLogger:
     ) -> None:
         assert opt.logger is not None
         assert opt.train is not None
+
         self.exp_name = opt.name
         self.interval = opt.logger.print_freq
         self.start_iter = start_iter
@@ -97,27 +98,23 @@ class MessageLogger:
         """Format logging message.
 
         Args:
-            log_vars (dict): It contains the following keys:
+            log_vars (dict): Contains the following keys:
                 epoch (int): Epoch number.
-                iter (int): Current iter.
-                lrs (list): List for learning rates.
-
-                time (float): Iter time.
-                data_time (float): Data time for each iter.
+                iter (int): Current iteration.
+                lrs (list): List of learning rates.
+                time (float): Iteration time.
+                data_time (float): Data loading time for each iteration.
         """
-        # epoch, iter, learning rates
+        # epoch, current iteration, and learning rates
         epoch = log_vars.pop("epoch")
         current_iter = log_vars.pop("iter")
         lrs = log_vars.pop("lrs")
 
-        message = (
-            f"[{self.exp_name[:5]}..][epoch:{epoch:3d}, iter:{current_iter:8,d}, lr:("
-        )
-        for v in lrs:
-            message += f"{v:.3e},"
-        message += ")] "
+        # Construct the base message with epoch, iteration, and learning rates
+        message = f"[epoch:{epoch:4,d}, iter:{current_iter:8,d}, lr:("
+        message += ", ".join([f"{v:.3e}" for v in lrs]) + ")] "
 
-        # time and estimated time
+        # performance, eta
         if "time" in log_vars.keys():
             iter_time = 1 / (log_vars.pop("time") * self.accum_iters)
             log_vars.pop("data_time")
@@ -126,19 +123,23 @@ class MessageLogger:
             time_sec_avg = total_time / (current_iter - self.start_iter + 1)
             eta_sec = time_sec_avg * (self.max_iters - current_iter - 1)
             eta_str = str(datetime.timedelta(seconds=int(eta_sec)))
-            message += f"[performance: {iter_time:.3f}] [eta: {eta_str}] "
 
-        # other items, especially losses
+            message += f"[performance: {iter_time:.3f} it/s] [eta: {eta_str}] "
+
+        # peak VRAM
+        message += (
+            f"[peak VRAM: {torch.cuda.max_memory_allocated() / (1024**3):.2f} GB] "
+        )
+
+        # Log any additional variables (typically losses)
         for k, v in log_vars.items():
             message += f"{k}: {v:.4e} "
-            # tensorboard logger
             if self.tb_logger is not None and "debug" not in self.exp_name:
                 label = f"losses/{k}" if k.startswith("l_") else k
-                self.tb_logger.add_scalar(
-                    label,
-                    v.to(dtype=torch.float32) if v.dtype == torch.bfloat16 else v,
-                    current_iter,
-                )
+                value = v.to(dtype=torch.float32) if v.dtype == torch.bfloat16 else v
+                self.tb_logger.add_scalar(label, value, current_iter)
+
+        # Log the final constructed message
         self.logger.info(message)
 
 
