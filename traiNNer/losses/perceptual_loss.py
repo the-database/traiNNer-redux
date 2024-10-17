4566f2fb457312d45db33d93790a7a410f650251
the-database <25811902+the-database@users.noreply.github.com>
2024-10-17 00:35:51 -0400

    test cast perceptual

--- traiNNer/losses/perceptual_loss.py
+++ traiNNer/losses/perceptual_loss.py
@@ -119,6 +119,7 @@ class PerceptualLoss(nn.Module):
         else:
             raise NotImplementedError(f"{criterion} criterion has not been supported.")
 
+    @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")  # pyright: ignore[reportAttributeAccessIssue] # https://github.com/pytorch/pytorch/issues/131765
     def forward(self, x: Tensor, gt: Tensor) -> tuple[Tensor | None, Tensor | None]:
         """Forward function.
 
