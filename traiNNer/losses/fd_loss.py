17a9602fdc6bd998379e2f6d5c01c70c6a07fde0
the-database <25811902+the-database@users.noreply.github.com>
2024-10-22 20:41:49 -0400

    Update fd_loss.py

--- traiNNer/losses/fd_loss.py
+++ traiNNer/losses/fd_loss.py
@@ -314,6 +314,11 @@ class FDLoss(nn.Module):
             y_mag = torch.abs(fft_y)
             y_phase = torch.angle(fft_y)
 
+            x_mag = x_mag.reshape(x_mag.shape[0], x_mag.shape[1], -1)  # B,N,M
+            y_mag = y_mag.reshape(y_mag.shape[0], y_mag.shape[1], -1)
+            x_phase = x_phase.reshape(x_phase.shape[0], x_phase.shape[1], -1)  # B,N,M
+            y_phase = y_phase.reshape(y_phase.shape[0], y_phase.shape[1], -1)
+
             s_amplitude = self.geomloss(x_mag, y_mag)
             s_phase = self.geomloss(x_phase, y_phase)
 
