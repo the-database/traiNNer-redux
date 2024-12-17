ee57f2720b0e507e0f8b99db530576e50409f994
the-database <25811902+the-database@users.noreply.github.com>
2024-12-17 12:23:56 -0500

    Update generate_schema.py

--- scripts/options/generate_schema.py
+++ scripts/options/generate_schema.py
@@ -1,8 +1,11 @@
 # Generate a JSON schema for yaml config file
+import os
+
 import msgspec
 from traiNNer.utils.redux_options import ReduxOptions
 
 schema = msgspec.json.schema(ReduxOptions)
-
-with open("schemas/redux-config.schema.json", "w") as schema_file:
+p = os.path.abspath("schemas/redux-config.schema.json")
+print(p)
+with open(p, "w") as schema_file:
     schema_file.write(msgspec.json.encode(schema).decode())
