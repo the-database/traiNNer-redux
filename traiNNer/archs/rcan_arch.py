328c4db553b1a956de3242a2ba796bdcffe79eab
the-database <25811902+the-database@users.noreply.github.com>
2024-12-23 02:22:33 -0500

    rcan b

--- traiNNer/archs/rcan_arch.py
+++ traiNNer/archs/rcan_arch.py
@@ -1,12 +1,36 @@
-# ruff: noqa
-# type: ignore
 import math
 from collections.abc import Callable
 
 import torch
-from torch import nn
+from spandrel.util import store_hyperparameters
+from torch import Tensor, nn
 
 from traiNNer.utils.registry import ARCH_REGISTRY
+
+
+def get_activation(activation: str = "relu") -> nn.Module:
+    """Get the specified activation layer.
+    Args:
+        activation (str): one of ``'relu'``, ``'leaky_relu'``, ``'elu'``, ``'gelu'``,
+            ``'swish'``, 'efficient_swish'`` and ``'none'``. Default: ``'relu'``
+    """
+    assert activation in [
+        "relu",
+        "leaky_relu",
+        "elu",
+        "silu",
+        "gelu",
+        "none",
+    ], f"Get unknown activation key {activation}"
+    activation_dict = {
+        "relu": nn.ReLU(inplace=True),
+        "leaky_relu": nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        "elu": nn.ELU(alpha=1.0, inplace=True),
+        "silu": nn.SiLU(inplace=True),
+        "gelu": nn.GELU(),
+        "none": nn.Identity(),
+    }
+    return activation_dict[activation]
 
 
 def default_conv(
@@ -18,11 +42,18 @@ def default_conv(
 
 
 class MeanShift(nn.Conv2d):
-    def __init__(self, rgb_range, rgb_mean, rgb_std, sign=-1) -> None:
+    def __init__(
+        self,
+        rgb_range: int,
+        rgb_mean: tuple[float, float, float],
+        rgb_std: tuple[float, float, float],
+        sign: float = -1,
+    ) -> None:
         super().__init__(3, 3, kernel_size=1)
         std = torch.Tensor(rgb_std)
         self.weight.data = torch.eye(3).view(3, 3, 1, 1)
         self.weight.data.div_(std.view(3, 1, 1, 1))
+        assert self.bias is not None
         self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean)
         self.bias.data.div_(std)
         self.requires_grad = False
@@ -37,7 +68,7 @@ class BasicBlock(nn.Sequential):
         stride: int = 1,
         bias: bool = False,
         bn: bool = True,
-        act=nn.ReLU(True),
+        act_mode: str | None = "relu",
     ) -> None:
         m: list[nn.Module] = [
             nn.Conv2d(
@@ -51,21 +82,21 @@ class BasicBlock(nn.Sequential):
         ]
         if bn:
             m.append(nn.BatchNorm2d(out_channels))
-        if act is not None:
-            m.append(act)
+        if act_mode is not None:
+            m.append(get_activation(act_mode))
         super().__init__(*m)
 
 
 class ResBlock(nn.Module):
     def __init__(
         self,
-        conv,
-        n_feat,
-        kernel_size,
-        bias=True,
-        bn=False,
-        act=nn.ReLU(True),
-        res_scale=1,
+        conv: Callable[..., nn.Conv2d],
+        n_feat: int,
+        kernel_size: int,
+        bias: bool = True,
+        bn: bool = False,
+        act_mode: str = "relu",
+        res_scale: float = 1,
     ) -> None:
         super().__init__()
         m = []
@@ -74,12 +105,12 @@ class ResBlock(nn.Module):
             if bn:
                 m.append(nn.BatchNorm2d(n_feat))
             if i == 0:
-                m.append(act)
+                m.append(get_activation(act_mode))
 
         self.body = nn.Sequential(*m)
         self.res_scale = res_scale
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         res = self.body(x).mul(self.res_scale)
         res += x
 
@@ -87,7 +118,15 @@ class ResBlock(nn.Module):
 
 
 class Upsampler(nn.Sequential):
-    def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True) -> None:
+    def __init__(
+        self,
+        conv: Callable[..., nn.Conv2d],
+        scale: int,
+        n_feat: int,
+        bn: bool = False,
+        act: nn.Module | None = None,
+        bias: bool = True,
+    ) -> None:
         m = []
         if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
@@ -111,7 +150,7 @@ class Upsampler(nn.Sequential):
 
 ## Channel Attention (CA) Layer
 class CALayer(nn.Module):
-    def __init__(self, channel, reduction: int = 16) -> None:
+    def __init__(self, channel: int, reduction: int = 16) -> None:
         super().__init__()
         # global average pooling: feature --> point
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
@@ -123,7 +162,7 @@ class CALayer(nn.Module):
             nn.Sigmoid(),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         y = self.avg_pool(x)
         y = self.conv_du(y)
         return x * y
@@ -133,14 +172,14 @@ class CALayer(nn.Module):
 class RCAB(nn.Module):
     def __init__(
         self,
-        conv,
-        n_feat,
-        kernel_size,
-        reduction,
-        bias=True,
-        bn=False,
-        act=nn.ReLU(True),
-        res_scale=1,
+        conv: Callable[..., nn.Conv2d],
+        n_feat: int,
+        kernel_size: int,
+        reduction: int,
+        bias: bool = True,
+        bn: bool = False,
+        act_mode: str = "relu",
+        res_scale: float = 1,
     ) -> None:
         super().__init__()
         modules_body = []
@@ -149,12 +188,12 @@ class RCAB(nn.Module):
             if bn:
                 modules_body.append(nn.BatchNorm2d(n_feat))
             if i == 0:
-                modules_body.append(act)
+                modules_body.append(get_activation(act_mode))
         modules_body.append(CALayer(n_feat, reduction))
         self.body = nn.Sequential(*modules_body)
         self.res_scale = res_scale
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         res = self.body(x)
         # res = self.body(x).mul(self.res_scale)
         res += x
@@ -164,11 +203,18 @@ class RCAB(nn.Module):
 ## Residual Group (RG)
 class ResidualGroup(nn.Module):
     def __init__(
-        self, conv, n_feat, kernel_size, reduction, act, res_scale, n_resblocks
+        self,
+        conv: Callable[..., nn.Conv2d],
+        n_feat: int,
+        kernel_size: int,
+        reduction: int,
+        act_mode: str,
+        res_scale: float,
+        n_resblocks: int,
     ) -> None:
         super().__init__()
         modules_body = []
-        modules_body = [
+        modules_body: list[nn.Module] = [
             RCAB(
                 conv,
                 n_feat,
@@ -176,45 +222,55 @@ class ResidualGroup(nn.Module):
                 reduction,
                 bias=True,
                 bn=False,
-                act=nn.ReLU(True),
-                res_scale=1,
+                act_mode=act_mode,
+                res_scale=res_scale,
             )
             for _ in range(n_resblocks)
         ]
         modules_body.append(conv(n_feat, n_feat, kernel_size))
         self.body = nn.Sequential(*modules_body)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         res = self.body(x)
         res += x
         return res
 
 
+@store_hyperparameters()
 @ARCH_REGISTRY.register()
 ## Residual Channel Attention Network (RCAN)
 class RCAN(nn.Module):
+    hyperparameters = {}  # noqa: RUF012
+
     def __init__(
         self,
+        *,
         scale: int = 4,
         n_resgroups: int = 10,
         n_resblocks: int = 20,
         n_feats: int = 64,
         n_colors: int = 3,
-        rgb_range: int = 255,  # TODO
+        rgb_range: int = 255,
+        norm: bool = True,
         kernel_size: int = 3,
         reduction: int = 16,
         res_scale: float = 1,
+        act_mode: str = "relu",
         conv: Callable[..., nn.Conv2d] = default_conv,
     ) -> None:
         super().__init__()
 
-        act = nn.ReLU(True)
-
-        # RGB mean for DIV2K
-        self.rgb_range = rgb_range
-        rgb_mean = (0.4488, 0.4371, 0.4040)
-        rgb_std = (1.0, 1.0, 1.0)
-        self.sub_mean = MeanShift(rgb_range, rgb_mean, rgb_std)
+        if norm:
+            # RGB mean for DIV2K
+            self.rgb_range = rgb_range
+            rgb_mean = (0.4488, 0.4371, 0.4040)
+            rgb_std = (1.0, 1.0, 1.0)
+            self.sub_mean = MeanShift(rgb_range, rgb_mean, rgb_std)
+            self.add_mean = MeanShift(rgb_range, rgb_mean, rgb_std, 1)
+        else:
+            self.rgb_range = 1
+            self.sub_mean = nn.Identity()
+            self.add_mean = nn.Identity()
 
         # define head module
         modules_head = [conv(n_colors, n_feats, kernel_size)]
@@ -226,7 +282,7 @@ class RCAN(nn.Module):
                 n_feats,
                 kernel_size,
                 reduction,
-                act=act,
+                act_mode=act_mode,
                 res_scale=res_scale,
                 n_resblocks=n_resblocks,
             )
@@ -237,17 +293,15 @@ class RCAN(nn.Module):
 
         # define tail module
         modules_tail = [
-            Upsampler(conv, scale, n_feats, act=False),
+            Upsampler(conv, scale, n_feats, act=None),
             conv(n_feats, n_colors, kernel_size),
         ]
-
-        self.add_mean = MeanShift(rgb_range, rgb_mean, rgb_std, 1)
 
         self.head = nn.Sequential(*modules_head)
         self.body = nn.Sequential(*modules_body)
         self.tail = nn.Sequential(*modules_tail)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         x *= self.rgb_range
         x = self.sub_mean(x)
         x = self.head(x)
@@ -259,78 +313,14 @@ class RCAN(nn.Module):
         x = self.add_mean(x)
         return x / self.rgb_range
 
-    def load_state_dict(self, state_dict, strict=False) -> None:
-        own_state = self.state_dict()
-        for name, param in state_dict.items():
-            if name in own_state:
-                if isinstance(param, nn.Parameter):
-                    param = param.data
-                try:
-                    own_state[name].copy_(param)
-                except Exception:
-                    if name.find("tail") >= 0:
-                        print("Replace pre-trained upsampler to new one...")
-                    else:
-                        raise RuntimeError(
-                            f"While copying the parameter named {name}, "
-                            f"whose dimensions in the model are {own_state[name].size()} and "
-                            f"whose dimensions in the checkpoint are {param.size()}."
-                        )
-            elif strict:
-                if name.find("tail") == -1:
-                    raise KeyError(f'unexpected key "{name}" in state_dict')
 
-        if strict:
-            missing = set(own_state.keys()) - set(state_dict.keys())
-            if len(missing) > 0:
-                raise KeyError(f'missing keys in state_dict: "{missing}"')
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_ng5_nb10(**kwargs):
-#     return RCAN(n_resblocks=10, n_resgroups=5, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_ng9_nb18(**kwargs):
-#     return RCAN(n_resblocks=18, n_resgroups=9, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_ng8_nb16(**kwargs):
-#     return RCAN(n_resblocks=16, n_resgroups=8, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_ng7_nb14(**kwargs):
-#     return RCAN(n_resblocks=14, n_resgroups=7, **kwargs)
+@ARCH_REGISTRY.register()
+def rcan_b(scale: int = 4, **kwargs) -> RCAN:
+    return RCAN(scale=scale, norm=False, reduction=1, **kwargs)
 
 
 @ARCH_REGISTRY.register()
-def RCAN_ng6_nb12(**kwargs):
-    return RCAN(n_resblocks=12, n_resgroups=6, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_ng20_nb40(**kwargs):
-#     return RCAN(n_resblocks=40, n_resgroups=20, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_r8(**kwargs):
-#     return RCAN(reduction=8, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_r4(**kwargs):
-#     return RCAN(reduction=4, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_r2(**kwargs):
-#     return RCAN(reduction=2, **kwargs)
-
-
-# @ARCH_REGISTRY.register()
-# def RCAN_r1(**kwargs):
-#     return RCAN(reduction=1, **kwargs)
+def rcan_b2(scale: int = 4, **kwargs) -> RCAN:
+    return RCAN(
+        scale=scale, n_resgroups=6, n_resblocks=12, norm=False, reduction=1, **kwargs
+    )
