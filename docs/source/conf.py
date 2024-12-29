2e13c619e4a564817d6d83af192ae36c0b409ced
the-database <25811902+the-database@users.noreply.github.com>
2024-12-28 19:03:35 -0500

    docs dark mode updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_rtd_dark_mode"]
+extensions = ["myst_parser"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
