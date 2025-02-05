1f4a3e82ced635dbf54a704c97abc5e43a7402d2
the-database <25811902+the-database@users.noreply.github.com>
2025-02-05 12:50:13 -0500

    restore pd l1 perceptual loss

---
+++ traiNNer/losses/perceptual_fp32_loss.py
@@ -0,0 +1,12 @@
+import torch
+from torch import Tensor
+
+from traiNNer.losses.perceptual_loss import PerceptualLoss
+from traiNNer.utils.registry import LOSS_REGISTRY
+
+
+@LOSS_REGISTRY.register()
+class PerceptualFP32Loss(PerceptualLoss):
+    @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")  # pyright: ignore[reportPrivateImportUsage] # https://github.com/pytorch/pytorch/issues/131765
+    def forward(self, x: Tensor, gt: Tensor) -> Tensor:
+        return super().forward(x, gt)
