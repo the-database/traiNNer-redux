60f3955d1f69c53bbff663e0ad4750806c7c345f
the-database <25811902+the-database@users.noreply.github.com>
2025-03-18 00:20:00 -0400

    Update msssim_loss.py

--- traiNNer/losses/msssim_loss.py
+++ traiNNer/losses/msssim_loss.py
@@ -22,13 +22,13 @@ import math
 import numpy as np
 import torch
 import torch.nn.functional as F  # noqa: N812
-from torch import nn
+from torch import Tensor, nn
 
 from traiNNer.archs.arch_util import to_2tuple
 from traiNNer.utils.registry import LOSS_REGISTRY
 
 
-def rgb2yiq(x: torch.Tensor) -> torch.Tensor:
+def rgb2yiq(x: Tensor) -> Tensor:
     r"""Convert a batch of RGB images to a batch of YIQ images
 
     Args:
@@ -52,7 +52,7 @@ def rgb2yiq(x: torch.Tensor) -> torch.Tensor:
     return x_yiq
 
 
-def rgb2lhm(x: torch.Tensor) -> torch.Tensor:
+def rgb2lhm(x: Tensor) -> Tensor:
     r"""Convert a batch of RGB images to a batch of LHM images
 
     Args:
@@ -73,7 +73,7 @@ def rgb2lhm(x: torch.Tensor) -> torch.Tensor:
     return x_lhm
 
 
-def rgb2ycbcr(x: torch.Tensor) -> torch.Tensor:
+def rgb2ycbcr(x: Tensor) -> Tensor:
     r"""Convert a batch of RGB images to a batch of YCbCr images
 
     It implements the ITU-R BT.601 conversion for standard-definition
@@ -103,8 +103,8 @@ def rgb2ycbcr(x: torch.Tensor) -> torch.Tensor:
 
 
 def to_y_channel(
-    img: torch.Tensor, out_data_range: float = 1.0, color_space: str = "yiq"
-) -> torch.Tensor:
+    img: Tensor, out_data_range: float = 1.0, color_space: str = "yiq"
+) -> Tensor:
     r"""Change to Y channel
     Args:
         image tensor: tensor with shape (N, 3, H, W) in range [0, 1].
@@ -128,7 +128,9 @@ def to_y_channel(
     return out_img
 
 
-def preprocess_rgb(x, test_y_channel, data_range: float = 1, color_space="yiq"):
+def preprocess_rgb(
+    x: Tensor, test_y_channel: bool, data_range: float = 1, color_space: str = "yiq"
+) -> Tensor:
     """
     Preprocesses an RGB image tensor.
 
@@ -155,7 +157,7 @@ def preprocess_rgb(x, test_y_channel, data_range: float = 1, color_space="yiq"):
     return x
 
 
-def symm_pad(im: torch.Tensor, padding: tuple[int, int, int, int]):
+def symm_pad(im: Tensor, padding: tuple[int, int, int, int]) -> Tensor:
     """Symmetric padding same as tensorflow.
     Ref: https://discuss.pytorch.org/t/symmetric-padding/19866/3
     """
@@ -165,7 +167,7 @@ def symm_pad(im: torch.Tensor, padding: tuple[int, int, int, int]):
     x_idx = np.arange(-left, w + right)
     y_idx = np.arange(-top, h + bottom)
 
-    def reflect(x, minx, maxx):
+    def reflect(x: np.ndarray, minx: float, maxx: float) -> np.ndarray:
         """Reflects an array around two points making a triangular waveform that ramps up
         and down,  allowing for pad lengths greater than the input length"""
         rng = maxx - minx
@@ -181,12 +183,18 @@ def symm_pad(im: torch.Tensor, padding: tuple[int, int, int, int]):
     return im[..., yy, xx]
 
 
-def exact_padding_2d(x, kernel, stride=1, dilation=1, mode="same"):
+def exact_padding_2d(
+    x: Tensor,
+    kernel: tuple | int,
+    stride: tuple | int = 1,
+    dilation: tuple | int = 1,
+    mode: str = "same",
+) -> Tensor:
     assert len(x.shape) == 4, f"Only support 4D tensor input, but got {x.shape}"
     kernel = to_2tuple(kernel)
     stride = to_2tuple(stride)
     dilation = to_2tuple(dilation)
-    b, c, h, w = x.shape
+    _b, _c, h, w = x.shape
     h2 = math.ceil(h / stride[0])
     w2 = math.ceil(w / stride[1])
     pad_row = (h2 - 1) * stride[0] + (kernel[0] - 1) * dilation[0] + 1 - h
@@ -219,14 +227,20 @@ class ExactPadding2d(nn.Module):
 
     """
 
-    def __init__(self, kernel, stride=1, dilation=1, mode="same") -> None:
+    def __init__(
+        self,
+        kernel: tuple,
+        stride: int = 1,
+        dilation: int = 1,
+        mode: str | None = "same",
+    ) -> None:
         super().__init__()
         self.kernel = to_2tuple(kernel)
         self.stride = to_2tuple(stride)
         self.dilation = to_2tuple(dilation)
         self.mode = mode
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         if self.mode is None:
             return x
         else:
@@ -235,7 +249,15 @@ class ExactPadding2d(nn.Module):
             )
 
 
-def imfilter(input, weight, bias=None, stride=1, padding="same", dilation=1, groups=1):
+def imfilter(
+    input: Tensor,
+    weight: Tensor,
+    bias: Tensor | None = None,
+    stride: int = 1,
+    padding: str = "same",
+    dilation: int = 1,
+    groups: int = 1,
+) -> Tensor:
     """imfilter same as matlab.
     Args:
         input (tensor): (b, c, h, w) tensor to be filtered
@@ -252,7 +274,7 @@ def imfilter(input, weight, bias=None, stride=1, padding="same", dilation=1, gro
     )
 
 
-def filter2(input, weight, shape="same"):
+def filter2(input: Tensor, weight: Tensor, shape: str = "same") -> Tensor:
     if shape == "same":
         return imfilter(input, weight, groups=input.shape[1])
     elif shape == "valid":
@@ -261,7 +283,12 @@ def filter2(input, weight, shape="same"):
         raise NotImplementedError(f"Shape type {shape} is not implemented.")
 
 
-def fspecial(size=None, sigma=None, channels=1, filter_type="gaussian"):
+def fspecial(
+    size: int | tuple,
+    sigma: float,
+    channels: int = 1,
+    filter_type: str = "gaussian",
+) -> Tensor:
     r"""Function same as 'fspecial' in MATLAB, only support gaussian now.
     Args:
         size (int or tuple): size of window
@@ -286,46 +313,46 @@ def fspecial(size=None, sigma=None, channels=1, filter_type="gaussian"):
 
 
 def ssim(
-    X,
-    Y,
-    win=None,
-    get_ssim_map=False,
-    get_cs=False,
-    get_weight=False,
-    downsample=False,
-    data_range=1.0,
-):
+    x: Tensor,
+    y: Tensor,
+    win: Tensor | None = None,
+    get_ssim_map: bool = False,
+    get_cs: bool = False,
+    get_weight: bool = False,
+    downsample: bool = False,
+    data_range: float = 1.0,
+) -> Tensor | tuple[Tensor, Tensor]:
     if win is None:
-        win = fspecial(11, 1.5, X.shape[1]).to(X)
+        win = fspecial(11, 1.5, x.shape[1]).to(x)
 
-    C1 = (0.01 * data_range) ** 2
-    C2 = (0.03 * data_range) ** 2
+    c1 = (0.01 * data_range) ** 2
+    c2 = (0.03 * data_range) ** 2
 
     # Averagepool image if the size is large enough
-    f = max(1, round(min(X.size()[-2:]) / 256))
+    f = max(1, round(min(x.size()[-2:]) / 256))
     # Downsample operation is used in official matlab code
     if (f > 1) and downsample:
-        X = F.avg_pool2d(X, kernel_size=f)
-        Y = F.avg_pool2d(Y, kernel_size=f)
+        x = F.avg_pool2d(x, kernel_size=f)
+        y = F.avg_pool2d(y, kernel_size=f)
 
-    mu1 = filter2(X, win, "valid")
-    mu2 = filter2(Y, win, "valid")
+    mu1 = filter2(x, win, "valid")
+    mu2 = filter2(y, win, "valid")
     mu1_sq = mu1.pow(2)
     mu2_sq = mu2.pow(2)
     mu1_mu2 = mu1 * mu2
-    sigma1_sq = filter2(X * X, win, "valid") - mu1_sq
-    sigma2_sq = filter2(Y * Y, win, "valid") - mu2_sq
-    sigma12 = filter2(X * Y, win, "valid") - mu1_mu2
+    sigma1_sq = filter2(x * x, win, "valid") - mu1_sq
+    sigma2_sq = filter2(y * y, win, "valid") - mu2_sq
+    sigma12 = filter2(x * y, win, "valid") - mu1_mu2
 
-    cs_map = (2 * sigma12 + C2) / (sigma1_sq + sigma2_sq + C2)
+    cs_map = (2 * sigma12 + c2) / (sigma1_sq + sigma2_sq + c2)
     cs_map = F.relu(
         cs_map
     )  # force the ssim response to be nonnegative to avoid negative results.
-    ssim_map = ((2 * mu1_mu2 + C1) / (mu1_sq + mu2_sq + C1)) * cs_map
+    ssim_map = ((2 * mu1_mu2 + c1) / (mu1_sq + mu2_sq + c1)) * cs_map
     ssim_val = ssim_map.mean([1, 2, 3])
 
     if get_weight:
-        weights = torch.log((1 + sigma1_sq / C2) * (1 + sigma2_sq / C2))
+        weights = torch.log((1 + sigma1_sq / c2) * (1 + sigma2_sq / c2))
         return ssim_map, weights
 
     if get_ssim_map:
@@ -347,11 +374,11 @@ class SSIMLoss(torch.nn.Module):
 
     def __init__(
         self,
-        channels=3,
-        downsample=False,
-        test_y_channel=True,
-        color_space="yiq",
-        crop_border=0.0,
+        channels: int = 3,
+        downsample: bool = False,
+        test_y_channel: bool = True,
+        color_space: str = "yiq",
+        crop_border: float = 0.0,
     ) -> None:
         super().__init__()
         self.downsample = downsample
@@ -361,33 +388,34 @@ class SSIMLoss(torch.nn.Module):
         self.data_range = 255
 
     @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")  # pyright: ignore[reportPrivateImportUsage] # https://github.com/pytorch/pytorch/issues/131765
-    def forward(self, X, Y):
-        assert X.shape == Y.shape, (
-            f"Input {X.shape} and reference images should have the same shape"
+    def forward(self, x: Tensor, y: Tensor) -> Tensor:
+        assert x.shape == y.shape, (
+            f"Input {x.shape} and reference images should have the same shape"
         )
 
         if self.crop_border != 0:
             crop_border = self.crop_border
-            X = X[..., crop_border:-crop_border, crop_border:-crop_border]
-            Y = Y[..., crop_border:-crop_border, crop_border:-crop_border]
+            x = x[..., crop_border:-crop_border, crop_border:-crop_border]
+            y = y[..., crop_border:-crop_border, crop_border:-crop_border]
 
-        X = preprocess_rgb(X, self.test_y_channel, self.data_range, self.color_space)
-        Y = preprocess_rgb(Y, self.test_y_channel, self.data_range, self.color_space)
+        x = preprocess_rgb(x, self.test_y_channel, self.data_range, self.color_space)
+        y = preprocess_rgb(y, self.test_y_channel, self.data_range, self.color_space)
 
-        score = ssim(X, Y, data_range=self.data_range, downsample=self.downsample)
+        score = ssim(x, y, data_range=self.data_range, downsample=self.downsample)
+        assert isinstance(score, Tensor)
         return score
 
 
 def ms_ssim(
-    X,
-    Y,
-    win=None,
-    data_range=1.0,
-    downsample=False,
-    test_y_channel=True,
-    is_prod=True,
-    color_space="yiq",
-):
+    x: Tensor,
+    y: Tensor,
+    win: Tensor | None = None,
+    data_range: float = 1.0,
+    downsample: bool = False,
+    test_y_channel: bool = True,
+    is_prod: bool = True,
+    color_space: str = "yiq",
+) -> Tensor:
     r"""Compute Multiscale structural similarity for a batch of images.
     Args:
         x: An input tensor. Shape :math:`(N, C, H, W)`.
@@ -399,18 +427,18 @@ def ms_ssim(
     Returns:
         Index of similarity between two images. Usually in [0, 1] interval.
     """
-    if not X.shape == Y.shape:
+    if not x.shape == y.shape:
         raise ValueError("Input images must have the same dimensions.")
 
-    weights = torch.FloatTensor([0.0448, 0.2856, 0.3001, 0.2363, 0.1333]).to(X)
+    weights = torch.FloatTensor([0.0448, 0.2856, 0.3001, 0.2363, 0.1333]).to(x)
 
     levels = weights.shape[0]
     mcs = []
 
     for _ in range(levels):
         ssim_val, cs = ssim(
-            X,
-            Y,
+            x,
+            y,
             win=win,
             get_cs=True,
             downsample=downsample,
@@ -418,20 +446,20 @@ def ms_ssim(
         )
 
         mcs.append(cs)
-        padding = (X.shape[2] % 2, X.shape[3] % 2)
-        X = F.avg_pool2d(X, kernel_size=2, padding=padding)
-        Y = F.avg_pool2d(Y, kernel_size=2, padding=padding)
+        padding = (x.shape[2] % 2, x.shape[3] % 2)
+        F.avg_pool2d(x, kernel_size=2, padding=padding)
+        F.avg_pool2d(y, kernel_size=2, padding=padding)
 
     mcs = torch.stack(mcs, dim=0)
 
     if is_prod:
         msssim_val = torch.prod((mcs[:-1] ** weights[:-1].unsqueeze(1)), dim=0) * (
-            ssim_val ** weights[-1]
+            ssim_val ** weights[-1]  # pyright: ignore[reportPossiblyUnboundVariable]
         )
     else:
         weights = weights / torch.sum(weights)
         msssim_val = torch.sum((mcs[:-1] * weights[:-1].unsqueeze(1)), dim=0) + (
-            ssim_val * weights[-1]
+            ssim_val * weights[-1]  # pyright: ignore[reportPossiblyUnboundVariable]
         )
 
     return msssim_val
@@ -455,11 +483,11 @@ class MSSSIMLoss(torch.nn.Module):
     def __init__(
         self,
         loss_weight: float,
-        channels=3,
-        downsample=False,
-        test_y_channel=True,
-        is_prod=True,
-        color_space="yiq",
+        channels: int = 3,
+        downsample: bool = False,
+        test_y_channel: bool = True,
+        is_prod: bool = True,
+        color_space: str = "yiq",
     ) -> None:
         super().__init__()
         self.loss_weight = loss_weight
@@ -470,7 +498,7 @@ class MSSSIMLoss(torch.nn.Module):
         self.data_range = 1
 
     @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")  # pyright: ignore[reportPrivateImportUsage] # https://github.com/pytorch/pytorch/issues/131765
-    def forward(self, X, Y):
+    def forward(self, x: Tensor, y: Tensor) -> Tensor:
         """Computation of MS-SSIM metric.
         Args:
             x: An input tensor. Shape :math:`(N, C, H, W)`.
@@ -478,17 +506,17 @@ class MSSSIMLoss(torch.nn.Module):
         Returns:
             Value of MS-SSIM metric in [0, 1] range.
         """
-        assert X.shape == Y.shape, (
+        assert x.shape == y.shape, (
             "Input and reference images should have the same shape, but got"
         )
-        f"{X.shape} and {Y.shape}"
+        f"{x.shape} and {y.shape}"
 
-        X = preprocess_rgb(X, self.test_y_channel, self.data_range, self.color_space)
-        Y = preprocess_rgb(Y, self.test_y_channel, self.data_range, self.color_space)
+        x = preprocess_rgb(x, self.test_y_channel, self.data_range, self.color_space)
+        y = preprocess_rgb(y, self.test_y_channel, self.data_range, self.color_space)
 
         score = ms_ssim(
-            X,
-            Y,
+            x,
+            y,
             data_range=self.data_range,
             downsample=self.downsample,
             is_prod=self.is_prod,
