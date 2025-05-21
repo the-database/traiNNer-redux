ae4b06657ff7c541bc49f494456c21a85bb72d1f
the-database <25811902+the-database@users.noreply.github.com>
2025-05-21 14:18:44 -0400

    Update logger.py

--- traiNNer/utils/logger.py
+++ traiNNer/utils/logger.py
@@ -343,7 +343,7 @@ def log_git_status() -> str | None:
                 ["git", "status", "-uno"], stderr=subprocess.DEVNULL
             ).decode("utf-8")
 
-            behind_flag = ""
+            behind_flag = " (Up to date)"
             if "Your branch is behind" in behind_output:
                 behind_flag = " ([yellow]updates available - please use [bold]git pull[/bold] to update[/yellow])"
         except Exception:
