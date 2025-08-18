f67ca4f7efef2be86993240376ce03eeaf8ebf6c
the-database <25811902+the-database@users.noreply.github.com>
2025-08-18 00:34:58 -0400

    fix fdat load state dict

--- traiNNer/archs/fdat_arch.py
+++ traiNNer/archs/fdat_arch.py
@@ -252,6 +252,15 @@ class FDAT(nn.Module):
         )
         self.apply(self._init_weights)
 
+    def load_state_dict(
+        self,
+        state_dict: StateDict,
+        *args,  # noqa: ANN002
+        **kwargs,
+    ) -> _IncompatibleKeys:
+        state_dict["upsampler.MetaUpsample"] = self.upsampler.MetaUpsample
+        return super().load_state_dict(state_dict, *args, **kwargs)
+
     def _init_weights(self, m: nn.Module) -> None:
         if isinstance(m, nn.Linear):
             trunc_normal_(m.weight, std=0.02)
