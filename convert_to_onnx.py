e2adcf26fe8ff6355ffcdbb26d86bcd15a650205
the-database <25811902+the-database@users.noreply.github.com>
2025-12-07 00:23:29 -0500

    onnx updates

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -3,6 +3,7 @@ import time
 from logging import Logger
 from os import path as osp
 from pathlib import Path
+from typing import Any
 
 import numpy as np
 import onnx
@@ -13,17 +14,27 @@ from onnxconverter_common.float16 import convert_float_to_float16
 from onnxslim import slim
 from rich.traceback import install
 from torch import Tensor
+from torch.export.dynamic_shapes import Dim
+from torch.onnx import ONNXProgram
 from traiNNer.models import build_model
 from traiNNer.models.base_model import BaseModel
 from traiNNer.utils.config import Config
 from traiNNer.utils.logger import clickable_file_path, get_root_logger
 from traiNNer.utils.redux_options import ReduxOptions
 
+MAX_LEGACY_OPSET = 20
+MIN_DYNAMO_OPSET = 18
+
 
 def get_out_path(
-    out_dir: str, name: str, opset: int, fp16: bool = False, optimized: bool = False
+    out_dir: str,
+    name: str,
+    opset: int,
+    fp16: bool,
+    optimized: bool,
+    dynamo: bool,
 ) -> str:
-    filename = f"{name}_fp{'16' if fp16 else '32'}_op{opset}{'_onnxslim' if optimized else ''}.onnx"
+    filename = f"{name}_fp{'16' if fp16 else '32'}_op{opset}{'_onnxslim' if optimized else ''}{'_dynamo' if dynamo else ''}.onnx"
     return osp.normpath(osp.join(out_dir, filename))
 
 
@@ -37,36 +48,82 @@ def convert_and_save_onnx(
     assert model.net_g is not None
     assert opt.onnx is not None
 
-    if opt.onnx.use_static_shapes:
-        dynamic_axes = None
-        input_names = None
-        output_names = None
-    else:
-        dynamic_axes = {
-            "input": {0: "batch_size", 2: "width", 3: "height"},
-            "output": {0: "batch_size", 2: "width", 3: "height"},
-        }
-        input_names = ["input"]
-        output_names = ["output"]
+    with torch.inference_mode():
+        if opt.onnx.dynamo:
+            opset = max(MIN_DYNAMO_OPSET, opt.onnx.opset)
 
-    out_path = get_out_path(out_dir, opt.name, opt.onnx.opset, False)
+            if opt.onnx.use_static_shapes:
+                dynamic_shapes = None
+            else:
+                dynamic_shapes = ((Dim.AUTO, Dim.STATIC, Dim.AUTO, Dim.AUTO),)
 
-    torch.onnx.export(
-        model.net_g,
-        (torch_input,),
-        out_path,
-        dynamo=opt.onnx.dynamo,
-        verbose=False,
-        opset_version=opt.onnx.opset,
-        dynamic_axes=dynamic_axes,
-        input_names=input_names,
-        output_names=output_names,
-    )
+            out_path = get_out_path(
+                out_dir,
+                opt.name,
+                opset,
+                fp16=False,
+                optimized=False,
+                dynamo=opt.onnx.dynamo,
+            )
+
+            onnx_program = torch.onnx.export(
+                model.net_g,
+                (torch_input,),
+                None,
+                dynamo=opt.onnx.dynamo,
+                verbose=False,
+                optimize=False,
+                opset_version=opset,
+                dynamic_shapes=dynamic_shapes,
+                verify=opt.onnx.verify,
+            )
+
+            assert onnx_program is not None
+            logger.info("Dynamo ONNX Conversion complete. Optimizing...")
+            onnx_program.optimize()
+
+            onnx_program.save(out_path)
+
+        else:
+            opset = min(MAX_LEGACY_OPSET, opt.onnx.opset)
+            if opt.onnx.use_static_shapes:
+                dynamic_axes = None
+                input_names = None
+                output_names = None
+            else:
+                input_names = ["input"]
+                output_names = ["output"]
+                dynamic_axes = {
+                    "input": {2: "height", 3: "width"},
+                    "output": {2: "height", 3: "width"},
+                }
+
+            out_path = get_out_path(
+                out_dir,
+                opt.name,
+                opset,
+                fp16=False,
+                optimized=False,
+                dynamo=opt.onnx.dynamo,
+            )
+
+            torch.onnx.export(
+                model.net_g,
+                (torch_input,),
+                out_path,
+                dynamo=opt.onnx.dynamo,
+                verbose=False,
+                optimize=False,
+                opset_version=opset,
+                input_names=input_names,
+                output_names=output_names,
+                verify=opt.onnx.verify,
+                dynamic_axes=dynamic_axes,
+            )
+
     model_proto = onnx.load(out_path)
-
     assert model_proto is not None
-
-    return model_proto, opt.onnx.opset, out_path
+    return model_proto, opset, out_path
 
 
 def verify_onnx(
@@ -86,11 +143,11 @@ def verify_onnx(
     onnx_output = ort_session.run(None, ort_inputs)
 
     try:
-        np.testing.assert_allclose(  # pyright: ignore # TODO onnx 1.18
+        np.testing.assert_allclose(
             torch_output_np,
-            onnx_output[0],  # pyright: ignore # TODO onnx 1.18
+            onnx_output[0],  # pyright: ignore
             rtol=1e-02,
-            atol=1e-03,  # pyright: ignore # TODO onnx 1.18
+            atol=1e-03,
         )
         logger.info("ONNX output verified against PyTorch output successfully.")
     except AssertionError as e:
@@ -130,10 +187,12 @@ def convert_pipeline(root_path: str) -> None:
         end_time - start_time,
     )
 
-    if opt.onnx.verify:
-        verify_onnx(model, logger, torch_input, out_path_fp32)
+    if not opt.onnx.dynamo:
+        if opt.onnx.verify:
+            verify_onnx(model, logger, torch_input, out_path_fp32)
 
     if opt.onnx.optimize:
+        logger.info("Optimizing ONNX with OnnxSlim...")
         model_proto = slim(
             model_proto,
         )
@@ -145,7 +204,7 @@ def convert_pipeline(root_path: str) -> None:
             ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
         )
         session_opt.optimized_model_filepath = get_out_path(
-            out_dir, opt.name, opset, False, True
+            out_dir, opt.name, opset, fp16=False, optimized=True, dynamo=opt.onnx.dynamo
         )
         ort.InferenceSession(out_path_fp32, session_opt)
         verify_onnx(model, logger, torch_input, session_opt.optimized_model_filepath)
@@ -153,7 +212,9 @@ def convert_pipeline(root_path: str) -> None:
 
     if opt.onnx.fp16:
         start_time = time.time()
-        out_path = get_out_path(out_dir, opt.name, opset, True, opt.onnx.optimize)
+        out_path = get_out_path(
+            out_dir, opt.name, opset, True, opt.onnx.optimize, opt.onnx.dynamo
+        )
         model_proto_fp16 = convert_float_to_float16(model_proto)
         onnx.save(model_proto_fp16, out_path)
         end_time = time.time()
