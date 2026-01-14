2a5dd96d765e65bf983d9ea1153189b268a3c895
the-database <25811902+the-database@users.noreply.github.com>
2026-01-13 20:22:00 -0500

    dat_2_l

--- traiNNer/archs/dat_arch.py
+++ traiNNer/archs/dat_arch.py
@@ -1229,6 +1229,92 @@ def dat_2(
 
 
 @SPANDREL_REGISTRY.register()
+def dat_2_l(
+    scale: int = 4,
+    in_chans: int = 3,
+    img_size: int = 64,
+    img_range: float = 1.0,
+    split_size: Sequence[int] = (8, 32),
+    depth: Sequence[int] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+    embed_dim: int = 180,
+    num_heads: Sequence[int] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+    expansion_factor: int = 2,
+    resi_connection: str = "1conv",
+    qkv_bias: bool = True,
+    qk_scale: float | None = None,
+    drop_rate: float = 0.0,
+    attn_drop_rate: float = 0.0,
+    drop_path_rate: float = 0.1,
+    use_chk: bool = False,
+    upsampler: str = "pixelshuffle",
+    unshuffle_mod: bool = False,
+) -> DAT:
+    return DAT(
+        upscale=scale,
+        in_chans=in_chans,
+        img_size=img_size,
+        img_range=img_range,
+        split_size=split_size,
+        depth=depth,
+        embed_dim=embed_dim,
+        num_heads=num_heads,
+        expansion_factor=expansion_factor,
+        resi_connection=resi_connection,
+        qkv_bias=qkv_bias,
+        qk_scale=qk_scale,
+        drop_rate=drop_rate,
+        attn_drop_rate=attn_drop_rate,
+        drop_path_rate=drop_path_rate,
+        use_chk=use_chk,
+        upsampler=upsampler,
+        unshuffle_mod=unshuffle_mod,
+    )
+
+
+@SPANDREL_REGISTRY.register()
+def dat_2_l_aligned(
+    scale: int = 4,
+    in_chans: int = 3,
+    img_size: int = 64,
+    img_range: float = 1.0,
+    split_size: Sequence[int] = (8, 32),
+    depth: Sequence[int] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+    embed_dim: int = 192,
+    num_heads: Sequence[int] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+    expansion_factor: int = 2,
+    resi_connection: str = "1conv",
+    qkv_bias: bool = True,
+    qk_scale: float | None = None,
+    drop_rate: float = 0.0,
+    attn_drop_rate: float = 0.0,
+    drop_path_rate: float = 0.1,
+    use_chk: bool = False,
+    upsampler: str = "pixelshuffle",
+    unshuffle_mod: bool = False,
+) -> DAT:
+    return DAT(
+        upscale=scale,
+        in_chans=in_chans,
+        img_size=img_size,
+        img_range=img_range,
+        split_size=split_size,
+        depth=depth,
+        embed_dim=embed_dim,
+        num_heads=num_heads,
+        expansion_factor=expansion_factor,
+        resi_connection=resi_connection,
+        qkv_bias=qkv_bias,
+        qk_scale=qk_scale,
+        drop_rate=drop_rate,
+        attn_drop_rate=attn_drop_rate,
+        drop_path_rate=drop_path_rate,
+        use_chk=use_chk,
+        upsampler=upsampler,
+        unshuffle_mod=unshuffle_mod,
+    )
+
+
+@SPANDREL_REGISTRY.register()
 def dat_light(
     scale: int = 4,
     in_chans: int = 3,
