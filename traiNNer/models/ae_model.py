8f37aa58b036a75c26f06214e6dffc283beac669
the-database <25811902+the-database@users.noreply.github.com>
2026-03-03 12:23:41 -0500

    Update ae_model.py

--- traiNNer/models/ae_model.py
+++ traiNNer/models/ae_model.py
@@ -270,7 +270,11 @@ class AEModel(BaseModel):
             )
 
     def optimize_parameters(
-        self, current_iter: int, current_accum_iter: int, apply_gradient: bool
+        self,
+        current_iter: int,
+        current_accum_iter: int,
+        apply_gradient: bool,
+        fidelity_only: bool = False,
     ) -> None:
         assert self.optimizer_ae is not None
         assert self.gt is not None
